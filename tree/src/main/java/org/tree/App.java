b4bcfe23a7b797581c98056c48f54f5fbdb3bd9a
yangyong <yangyong@primeton.com>
2016-09-10 21:08:25 +0800

    Add: 增加初始化项目

---
+++ tree/src/main/java/org/tree/App.java
@@ -0,0 +1,13 @@
+package org.tree;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
